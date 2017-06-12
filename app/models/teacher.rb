553e1f4f84b6c606e96ab0304fcffe4f751ec0bd
Adam Gavlák <adam.gavlak@st.fri.uniza.sk>
2017-06-12 18:56:22 +0200

    Add teacher total method

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -14,6 +14,31 @@ class Teacher < ApplicationRecord
     ids = self.teacher_courses.select{ |c| c.is_lecturer }.map{|i| i.course_id}
   end
 
+  def total(course)
+    t = 0
+    lecture = Multiplier.find_by(name: 'Prednáška').value
+    classes = Multiplier.find_by(name: 'Cvičenie').value
+    labs = Multiplier.find_by(name: 'Laboratórne cvičenie').value
+    weeks = 13
+
+    self.teacher_courses.each do |tc|
+      if tc.course.id == course.id
+        if tc.is_lecturer
+          t += tc.course.lectures_weekly * lecture * weeks
+        end
+
+        tc.course.groups.each do |group|
+          if group.teacher_group_courses?(self.id, tc.course_id)
+            t += tc.course.classes_weekly * classes * weeks
+            t += tc.course.lab_classes_weekly * labs * weeks
+          end
+        end
+      end
+    end
+
+    t
+  end
+
   # Migration
   # t.string :name
   # t.string :email
